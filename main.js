57043ce1f60acca02c1955a580ca3c2410639b6a
idryzhakov <ildryzhakov@gmail.com>
2020-10-19 18:08:25 +0300

    Reworked ToDo, made with massive of objects

--- main.js
+++ main.js
@@ -1,111 +1,127 @@
-const ToDoInput = document.getElementById('todo-input');
-const ToDoItem = document.getElementById('todo-item');
+const toDoInput = document.getElementById('todo-input');
+const toDoItem = document.getElementById('todo-item');
 const btnAll = document.getElementById('btn-all');
 const btnActive = document.getElementById('btn-active');
 const btnComp = document.getElementById('btn-completed');
 const btnClear = document.getElementById('clear-btn');
-let arrItems = [];
+
+let id = 0;
+let TaskItemList = [];
 
 
 btnComp.addEventListener('click',function(evt) {
 
-    arrItems.forEach(function(item, index) {
+    let toDoItems = document.querySelectorAll('.todo-item');
 
-      const checkIcon =  item.querySelector('.check-icon');
-
-            if (!checkIcon.classList.contains('visible')) {
-                    item.style.display = 'none';
-            } else if (checkIcon.classList.contains('visible')) {
-                    item.style.display = 'block';
+    toDoItems.forEach((item,index)=>{
+        TaskItemList.forEach((it,ind) => {
+            if (it.id == item.id && it.completed === false )  {
+                item.style.display = 'none';
+            }  else  if (it.id == item.id && it.completed === true ){
+                item.style.display = 'block';
             }
+        });
     });
-
 });
 btnClear.addEventListener('click',function(evt) {
 
-    while (arrItems.some((item) => item.querySelector('.check-icon').classList.contains('visible') )) {
-
-        arrItems.forEach(function(item,index) {
-
-            const checkIcon = item.querySelector('.check-icon');
-
-             if (checkIcon.classList.contains('visible')) {
-                item.style.display = 'none';
-                arrItems.splice(index,1);
-             }
+        TaskItemList.forEach((item,index) => {
+            if (item.completed == true) {            
+               let delElem = document.getElementById(TaskItemList[index].id);
+               delElem.parentNode.removeChild(delElem);
+                delete TaskItemList[index];
+            }
     
         })
-    }
+    
 });
-
 
 btnAll.addEventListener('click',function(evt) {
 
-    arrItems.forEach(function(item,index) {
-        
+    document.querySelectorAll('.todo-item').forEach((item,index) => {
         item.style.display = 'block';
-
     });
 
 });
 
 btnActive.addEventListener('click',function(evt) {
 
-    arrItems.forEach(function(item, index) {
+    let toDoItems = document.querySelectorAll('.todo-item');
 
-        const checkIcon =  item.querySelector('.check-icon');
-
-            if (checkIcon.classList.contains('visible')) {
-                    item.style.display = 'none';
-            } else if (!checkIcon.classList.contains('visible')) {
-                    item.style.display = 'block';
+    toDoItems.forEach((item,index)=>{
+        TaskItemList.forEach((it,ind) => {
+            if (it.id == item.id && it.completed === false )  {
+                item.style.display = 'block';
+            }  else  if (it.id == item.id && it.completed === true ){
+                item.style.display = 'none';
             }
+        });
     });
 
 });
 
-ToDoInput.addEventListener('keydown', function(evt) {
+toDoInput.addEventListener('keydown', function(evt) {
     
-    const val = ToDoInput.value;
+    const val = toDoInput.value;
 
     if (evt.key === 'Enter') {
 
-        const CloneItem = document.getElementById('todo-item').cloneNode(true);
-        const circle = CloneItem.querySelector('.circle'); 
-        const cancel = CloneItem.querySelector('.cancel-icon');
+        let ToDoItem = document.createElement('div');
+        ToDoItem.className = "todo-item";
+        ToDoItem.innerHTML = ' <div class="circle" id="circle" ><span class="check-icon"></span></div><span class="todo-text">item-1</span><span class="cancel-icon"></span>';
+        ToDoItem.setAttribute('id',id);
+        const circle = ToDoItem.querySelector('.circle'); 
+        const cancel = ToDoItem.querySelector('.cancel-icon');
+        ToDoItem.querySelector('.todo-text').textContent = val;
 
-        CloneItem.style.display = 'block';
-        CloneItem.querySelector('.todo-text').textContent = val;
-        circle.nextElementSibling.classList.remove('line');
-        circle.firstChild.classList.remove('visible');
+        if (val === '') {
+            alert('Input cannot be empty')
+        } else {
 
-        const ExistingTask = arrItems.some((item)  =>  val === item.querySelector('.todo-text').textContent) ;
+        TaskItem = {
+            taskItemText: val,
+            id: id,
+            completed: false
+        }
+        
+        id++;
+        TaskItemList.push(TaskItem);
+        toDoInput.after(ToDoItem);
 
-        if (ExistingTask) {
-
-            alert('this task already exist');
-            ToDoInput.style.boxShadow = ' 0 0 10px red';
-
-        } else if (val !== '') {
-
-        ToDoInput.style.boxShadow = 'none';
-
-        arrItems.push(CloneItem);
-
-        ToDoInput.after(CloneItem);
-
-        circle.addEventListener('click',function() {
+        circle.addEventListener('click',function(evt) {
            circle.firstChild.classList.toggle('visible');
            circle.nextElementSibling.classList.toggle('line');
+           curId = circle.parentElement.id;
+
+            if (circle.firstChild.classList.contains('visible')) {
+                TaskItemList.forEach((item,index) => {
+                    if (item.id == curId) {
+                       item.completed = true;
+                    } 
+               });
+            } else {
+                TaskItemList.forEach((item,index) => {
+                    if (index == curId) {
+                       item.completed = false;
+                    } 
+               });
+            }
+
         });
 
         cancel.addEventListener('click',function() {
            
-            const ToDoText = CloneItem.querySelector('.todo-text').textContent;
-            CloneItem.style.display = 'none';
+           curId = cancel.parentElement.id;
+           delElem = cancel.parentNode;
 
-           arrItems.forEach((item,index) => { if ( ToDoText === item.querySelector('.todo-text').textContent) {arrItems.splice(index,1);}}
-           );
+           delElem.parentNode.removeChild(delElem);
+           TaskItemList.forEach((item,index) => {
+
+                if (curId === item.id) {
+                    TaskItemList.splice(curId,1);
+                }
+
+           });
         });
         }
     }
