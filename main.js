890db0a46f2f66830d82da05f76002b8c559f963
idryzhakov <ildryzhakov@gmail.com>
2020-10-21 14:03:19 +0300

    final version of toDoList

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const toDoInput = document.getElementById("todo-input");
-const btnAll = document.getElementById("btn-all");
-const btnActive = document.getElementById("btn-active");
-const btnComp = document.getElementById("btn-completed");
-const btnClear = document.getElementById("clear-btn");
+const toDoInput = document.getElementById('todo-input');
+const btnAll = document.getElementById('btn-all');
+const btnActive = document.getElementById('btn-active');
+const btnComp = document.getElementById('btn-completed');
+const btnClear = document.getElementById('clear-btn');
 
 let id = 0;
 let taskItemList = [];
@@ -11,8 +11,8 @@ function comparing(arr, item, id, stateFirst, stateSecond) {
   arr.includes(id)? (item.style.display = stateFirst) : (item.style.display = stateSecond);
 }
 
-btnComp.addEventListener("click", function (evt) {
-  let toDoItems = document.querySelectorAll(".todo-item");
+btnComp.addEventListener('click', function (evt) {
+  let toDoItems = document.querySelectorAll('.todo-item');
 
   let compArr = taskItemList.map((item) => {
     if (item.completed) {
@@ -25,8 +25,8 @@ btnComp.addEventListener("click", function (evt) {
   });
 });
 
-btnClear.addEventListener("click", function (evt) {
-  let toDoItems = document.querySelectorAll(".todo-item");
+btnClear.addEventListener('click', function (evt) {
+  let toDoItems = document.querySelectorAll('.todo-item');
   let compArr = taskItemList.map((item) => {
     if (item.completed) {
       return item.id;
@@ -34,25 +34,21 @@ btnClear.addEventListener("click", function (evt) {
   });
   toDoItems.forEach((item) => {
     idNumb = parseInt(item.id, 10);
-     if (compArr.includes(idNumb)) {
-        let delElem = document.getElementById(item.id);
-        delElem.parentNode.removeChild(delElem);
-     }
-  });
-  taskItemList = taskItemList.filter((item) => {
-    if (!(compArr.includes(item.id))) {
-        return item;
+    if (compArr.includes(idNumb)) {
+      let delElem = document.getElementById(item.id);
+      delElem.parentNode.removeChild(delElem);
     }
   });
+  taskItemList = taskItemList.filter((item) => !compArr.includes(item.id));
 });
 
-btnAll.addEventListener("click", (evt) => {
-  document.querySelectorAll(".todo-item").forEach((item) => {
-    item.style.display = "block";
+btnAll.addEventListener('click', (evt) => {
+  document.querySelectorAll('.todo-item').forEach((item) => {
+    item.style.display = 'block';
   });
 });
 
-btnActive.addEventListener("click", (evt) => {
+btnActive.addEventListener('click', (evt) => {
   let toDoItems = document.querySelectorAll(".todo-item");
 
   let compArr = taskItemList.map((item) => {
@@ -69,51 +65,44 @@ btnActive.addEventListener("click", (evt) => {
 toDoInput.addEventListener("keydown", function (evt) {
   const val = toDoInput.value;
 
-  if (evt.key === "Enter") {
+  if (evt.key === 'Enter') {
     let toDoItem = document.createElement("div");
     toDoItem.className = "todo-item";
     toDoItem.innerHTML =
-      '<div class="circle" id="circle" ><span class="check-icon"></span></div><span class="todo-text">item-1</span><span class="cancel-icon"></span>';
+      "<div class='circle' id='circle' ><span class='check-icon'></span></div><span class='todo-text'>item-1</span><span class='cancel-icon'></span>";
     toDoItem.setAttribute("id", id);
     const circle = toDoItem.querySelector(".circle");
     const cancel = toDoItem.querySelector(".cancel-icon");
     toDoItem.querySelector(".todo-text").textContent = val;
 
-    if (val === "") {
-      alert("Input cannot be empty");
+    if (!val) {
+      alert('Input cannot be empty');
     } else {
       taskItem = {
         taskItemText: val,
         id: id,
         completed: false,
       };
-
+      toDoInput.value = "";
       id++;
       taskItemList.push(taskItem);
       toDoInput.after(toDoItem);
-
-      circle.addEventListener("click", function (evt) {
-        circle.firstChild.classList.toggle("visible");
-        circle.nextElementSibling.classList.toggle("line");
+      circle.addEventListener('click', function (evt) {
+        circle.firstChild.classList.toggle('visible');
+        circle.nextElementSibling.classList.toggle('line');
         curId = parseInt(circle.parentElement.id, 10);
 
         taskItemList.forEach((item, index) => {
           if (item.id === curId) {
-            item.completed = circle.firstChild.classList.contains("visible");
+            item.completed = circle.firstChild.classList.contains('visible');
           }
         });
       });
-
-      cancel.addEventListener("click", function () {
+      cancel.addEventListener('click', function () {
         curId = parseInt(cancel.parentElement.id, 10);
         delElem = cancel.parentNode;
         delElem.parentNode.removeChild(delElem);
-        console.log(taskItemList);
-        taskItemList = taskItemList.filter((item, index) => {
-          if (!(curId === item.id)) {
-            return item;
-          }
-        });
+        taskItemList = taskItemList.filter((item) => item.id !== curId);
       });
     }
   }
